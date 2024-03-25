19d6339163e761e22c34df0fea929476546dab59
Bao Nguyen <nnbao204@gmail.com>
2024-03-25 01:05:01 -0400

    update checkbox

--- app.py
+++ app.py
@@ -10,20 +10,30 @@ def upload_form():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return render_template('upload.html', graph_path=None, error="No file part")
-        file = request.files['file']
-        if file.filename == '':
-            return render_template('upload.html', graph_path=None, error="No selected file")
-        if file:
-            filename = 'uploads/' + file.filename
-            file.save(filename)
-            N = int(request.form['N'])
-            L = int(request.form['L'])
-            M = int(request.form['M'])
-            r = int(request.form['r'])
-            graph_path = generate_inf_graph(filename, r, M, N, L)
-            return render_template('upload.html', graph_path=graph_path)
+        use_previous = request.form.get('use_previous') == 'on'
+
+        if use_previous:
+            # Use previously uploaded dataset
+            filename = 'uploads/dataset.txt'
+        else:
+            # Handle file upload as before
+            if 'file' not in request.files:
+                return render_template('upload.html', graph_path=None, error="No file part")
+            file = request.files['file']
+            if file.filename == '':
+                return render_template('upload.html', graph_path=None, error="No selected file")
+            if file:
+                filename = 'uploads/' + file.filename
+                file.save(filename)
+
+        # Process parameters and generate graph
+        N = int(request.form['N'])
+        L = int(request.form['L'])
+        M = int(request.form['M'])
+        r = int(request.form['r'])
+        graph_path = generate_inf_graph(filename, r, M, N, L)
+
+        return render_template('upload.html', graph_path=graph_path)
 
 @app.route('/plot/<path:path>')
 def serve_plot(path):
